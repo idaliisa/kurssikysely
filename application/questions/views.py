d4f71483037ca2c6a41abe922d099b29f4e978a0
idaliisa <idaliisa@gmail.com>
2018-08-03 23:28:19 +0300

    kysymystä voi muokata

--- application/questions/views.py
+++ application/questions/views.py
@@ -10,6 +10,22 @@ def questions_index():
 def questions_form():
     return render_template("questions/new.html")
 
+@app.route("/questions/update/", methods=["POST"])
+def questions_update():
+    id = request.form.get("question_id", type=int)
+    return render_template("questions/update.html", q = Question.query.get(id))
+
+@app.route("/questions/save/", methods=["POST"])
+def question_save():
+    #q = Question(request.form.get("id"), request.form.get("nimi"),request.form.get("kysymystyyppi"))
+    id = request.form.get("question_id", type=int)
+    q = Question.query.get(id)
+    q.kysymys = request.form.get("nimi")
+    q.kysymystyyppi = request.form.get("kysymystyyppi")
+    db.session().commit()
+
+    return redirect(url_for("questions_index"))
+
 @app.route("/questions/", methods=["POST"])    
 def questions_create():
     q = Question(request.form.get("nimi"),request.form.get("kysymystyyppi"))
