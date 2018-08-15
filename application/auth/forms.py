19ffae6168c57470b439b3be2b828a003b21e73e
idaliisa <idaliisa@gmail.com>
2018-08-15 22:40:50 +0300

    paranneltu lomakkeiden validointia

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -2,16 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SelectField, validators
   
 class LoginForm(FlaskForm):
-    kayttajatunnus = StringField("käyttäjätunnus", [validators.Length(min=5)])
-    salasana = PasswordField("salasana", [validators.Length(min=5)])
+    kayttajatunnus = StringField("käyttäjätunnus", [validators.Length(min=5, max=50)])
+    salasana = PasswordField("salasana", [validators.Length(min=5, max=50)])
   
     class Meta:
         csrf = False
 
 class RegistrationForm(FlaskForm):
-    nimi = StringField("nimi", [validators.Length(min=5)])
-    kayttajatunnus = StringField("käyttäjätunnus", [validators.Length(min=5)])
-    salasana = PasswordField("salasana", [validators.Length(min=5)])
+    nimi = StringField("nimi", [validators.Length(min=5, max=50)])
+    kayttajatunnus = StringField("käyttäjätunnus", [validators.Length(min=5, max=50)])
+    salasana = PasswordField("salasana", [validators.Length(min=5, max=50)])
     kayttajatyyppi = SelectField("käyttäjätyyppi", choices=[("text", "pääkäyttäjä"), ("text", "laitoksen vastuuhenkilö"),("text", "opettaja"),("text", "oppilas")])
 
     class Meta:
